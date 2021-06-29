617b22481d08512eab65fc813147af7bb818dbc2
Ungala <72914489+ungala1@users.noreply.github.com>
2021-06-30 00:39:27 +0400

    laravel setup

---
+++ database/migrations/2021_06_29_201926_create_test_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTestUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('test_users', function (Blueprint $table) {
+            $table->id();
+            $table->integer('score');
+            $table->bigInteger('user_id');
+            $table->bigInteger('test_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('test_users');
+    }
+}
