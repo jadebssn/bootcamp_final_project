599c7f28876db0f2ddde60adeda5e2a243ce6d9e
Mihael <mihaellipotic6@gmail.com>
2021-07-13 15:00:20 +0200

    options

--- resources/js/Option/Option.jsx
+++ resources/js/Option/Option.jsx
@@ -1,38 +1,5 @@
-import { useState } from "react";
 
 export default function Options(props) {
-    
-
-    const [optionId, setOptionId] = useState(0)
-    const [className, setClassName] = useState("quiz-answer")
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const token = localStorage.getItem('my_token');
-
-        const response = await fetch(`/api/submit/${optionId}`, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-type': 'application/json',
-                'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
-                'Authorization': `Bearer ${token}`
-            },
-        })
-
-        const data = await response.json();
-
-        if (data.errors) {
-            // display errors ...
-        }
-    }
-
-    console.log(optionId)
-    const changingBackground = () => {
-        if (className === "quiz-answer") {
-            setClassName ("quiz-answer-selected")
-        } else {setClassName("quiz-answer")}
-    }
 
     return (
         <div>
@@ -40,16 +7,16 @@ export default function Options(props) {
             {
                 props.question.options.map((option, i) => (
                     <li
-                        className={ option.id === optionId ? "quiz-answer-selected" :  "quiz-answer" }
+                        className={ option.id === props.listOptionId ? "quiz-answer-selected" :  "quiz-answer" }
                         key={option.id}
                         onClick={()=> {
-                            setOptionId(option.id);
+                           props.setListOptionId([...props.listOptionId, option.id]);
                         }}   
                     >{option.text}</li>
                 ))
             }
             </ul>
-            <span><button className="button" onClick={handleSubmit}>Submit answer</button></span>
+          
         </div>
     )
 }
--- resources/js/Question/Question.jsx
+++ resources/js/Question/Question.jsx
@@ -1,18 +1,45 @@
 import Option from "../Option/Option";
+import { useState } from "react";
 
 export default function Question(props) {
 
-    console.log(props.test.questions)
+    const [listOptionId, setListOptionId] = useState([])
+
+
+ 
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const token = localStorage.getItem('my_token');
+
+        const response = await fetch(`/api/submit/${listOptionId}`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json',
+                'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+                'Authorization': `Bearer ${token}`
+            },
+        })
+
+        const data = await response.json();
+
+        if (data.errors) {
+            // display errors ...
+        }
+    }
+    console.log(listOptionId);
  
     return (
         <div className="quiz-question">
             {props.test.questions?.map((question, i) => (
                 <div key={i}>
                     <p>{question.text}</p>
-                    <Option question={question} />
+                    <Option question={question} setListOptionId={setListOptionId} listOptionId={listOptionId}
+                    />
                 </div>
             ))}
-            
+            <span><button className="button" onClick={handleSubmit}>Submit answer</button></span>
            
         </div>
     )
