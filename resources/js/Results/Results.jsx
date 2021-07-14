bc525f5df29ef3da2ee9be8955dee04e5680a6b8
Jade <jade.besson@hotmail.fr>
2021-07-14 11:14:07 +0200

    result component started

--- resources/js/Results/Results.jsx
+++ resources/js/Results/Results.jsx
@@ -1,3 +1,22 @@
+import { useState, useEffect } from "react";
 export default function Results() {
-    
+    const [course, setCourse] = useState(null)
+
+        const loadCourse = async () => {
+            const response = await fetch(`/api/course/${id}`, {
+                headers: {
+                    "Accept": "application/json"
+                }
+            });
+            const data = await response.json();
+
+            setCourse(data);
+        }
+
+        useEffect(() => {
+
+            loadCourse();
+
+        }, [id]);
+
 }
